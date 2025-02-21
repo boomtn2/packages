bca58f03d6568e485fde3de19676171bad7bbb84
hit <hieuconit@gmail.com>
2025-02-21 15:12:06 +0700

    [Interface] getVideoResolution

--- packages/video_player/video_player_platform_interface/lib/video_player_platform_interface.dart
+++ packages/video_player/video_player_platform_interface/lib/video_player_platform_interface.dart
@@ -83,6 +83,12 @@ abstract class VideoPlayerPlatform extends PlatformInterface {
     throw UnimplementedError('changeBandWidth() has not been implemented.');
   }
 
+  /// getVideoResolution
+  Future<List<VideoResolution>> getVideoResolution(int textureId) {
+    throw UnimplementedError('changeBandWidth() has not been implemented.');
+  }
+
+
   /// Sets the video position to a [Duration] from the start.
   Future<void> seekTo(int textureId, Duration position) {
     throw UnimplementedError('seekTo() has not been implemented.');
@@ -111,6 +117,10 @@ abstract class VideoPlayerPlatform extends PlatformInterface {
   /// Sets additional options on web
   Future<void> setWebOptions(int textureId, VideoPlayerWebOptions options) {
     throw UnimplementedError('setWebOptions() has not been implemented.');
+  }
+
+  Widget buildView(int textureId) {
+    throw UnimplementedError('buildView() has not been implemented.');
   }
 }
 
@@ -478,4 +488,19 @@ class VideoPlayerWebOptionsControls {
 
     return controlsList.join(' ');
   }
+}
+
+
+class VideoResolution {
+  VideoResolution({
+    required this.width,
+    required this.height,
+    required this.bitRate,
+  });
+
+  int width;
+
+  int height;
+
+  int bitRate;
 }
